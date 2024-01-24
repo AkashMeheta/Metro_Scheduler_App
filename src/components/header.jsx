e953f3c67fe2b680654182366d1017ec9f699ff9
Akash Meheta <akashmeheta11@gmail.com>
2024-01-24 16:06:50 +0530

    Nothing

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,8 +13,8 @@ const HeaderSection = () => {
             <div className="container">
                 <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
                     <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                        <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
-                        <span className="fs-4"><h2>Metro Partner</h2></span>
+                        {/* <img src="./train-moves-fast-dusk-showing-speed-modern-travel.jpg" style={{width: "50px", height: "50px"}} alt="" /> */}
+                        <span className="fs-4"><h2>Metro-Minder</h2></span>
                     </a>
 
                     <ul className="nav nav-pills">
